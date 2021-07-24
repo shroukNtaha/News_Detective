1fb0dfb95bb9302c3e19deb7308f5daac6e36259
lubnahassan <lubnahassan35@gmail.com>
2021-07-24 23:55:50 +0200

    notification button

--- lib/services/notificationService.dart
+++ lib/services/notificationService.dart
@@ -9,4 +9,14 @@ class NotificationService {
   Future add(NotificationOLD data) async {
     return await _rep.addDocument(data.toJson());
   }
+
+  Future push() async {
+    // return await db.
+  }
+  //Listen for any change on document marie in collection users
+  //const functions = require('firebase-functions');
+  //NotificationService.myFunctionName = functions.firestore
+  //  .document('news').onWrite((change, context) => {
+  // ... Your code here
+  // });
 }
