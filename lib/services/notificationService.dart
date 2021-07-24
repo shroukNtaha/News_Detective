3e3886a2631bd498777f006995823248e9c92aa8
lubnahassan <lubnahassan35@gmail.com>
2021-07-24 02:14:08 +0200

    conflict

--- lib/services/notificationService.dart
+++ lib/services/notificationService.dart
@@ -1,11 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:news_detective/common/Repository.dart';
+import 'package:news_detective/models/notification.dart';
 
 class NotificationService {
-  //Repository _rep = Repository("notification");
+  Repository _rep = Repository("notification");
   final db = FirebaseFirestore.instance.collection("notification");
 
   Future add(NotificationOLD data) async {
     return await _rep.addDocument(data.toJson());
   }
-
 }
