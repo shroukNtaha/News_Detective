041d05abb93af36a679052414f40c11bbd19fa11
lubnahassan <lubnahassan35@gmail.com>
2021-07-24 02:10:17 +0200

    handle current user

--- lib/services/notificationService.dart
+++ lib/services/notificationService.dart
@@ -1,9 +1,7 @@
-import 'package:news_detective/common/Repository.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:news_detective/models/notification.dart';
 
 class NotificationService {
-  Repository _rep = Repository("notification");
+  //Repository _rep = Repository("notification");
   final db = FirebaseFirestore.instance.collection("notification");
 
   // Future add(Notification data) async {
